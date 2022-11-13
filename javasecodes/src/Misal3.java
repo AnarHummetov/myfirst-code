8dba0f701efdaeb3770353897ee42b82d3ee1b08
AnarHummetov <117859842+AnarHummetov@users.noreply.github.com>
2022-11-13 14:57:12 +0400

    Ders3_KvadratTenlik

---
+++ javasecodes/src/Misal3.java
@@ -0,0 +1,8 @@
+
+public class Misal3 {
+public static void main(String[] args) {
+	char c = 'A';
+	int kodu=(int) c;
+	System.out.println(kodu);
+}
+}
---
+++ javasecodes/src/StringMisal.java
@@ -0,0 +1,11 @@
+
+public class StringMisal {
+	public static void main(String[] args) {
+		String name = "Anar";
+		String name2 = "Vadim";
+		String name3 = "Eli";
+		System.out.println(name);
+		System.out.println(name2);
+		System.out.println(name3);
+	}
+}
