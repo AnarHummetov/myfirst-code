5b3c26f3ecdd2c08a1a553cc95d714acfa24da8b
AnarHummetov <117859842+AnarHummetov@users.noreply.github.com>
2022-11-24 17:56:57 +0400

    DovrlerEv-isi

---
+++ javasecodes/src/ders5/dovrler/DovrlerMisal.java
@@ -0,0 +1,20 @@
+package ders5.dovrler;
+
+public class DovrlerMisal {
+	public static void main(String[] args) {
+		System.out.println("1-den 100-e qeder olan cut ededler");
+		for (int a = 0; a <= 100; a += 2) {
+			System.out.println(a);
+
+		}
+		System.out.println("15 ve 40 araliginda olan ededler");
+		for (int i = 15; i <= 40; i++) {
+			System.out.println(i);
+
+		}
+		System.out.println("20 ve 50 araliginda olan tek ededler");
+		for (int b = 21; b <= 50; b += 2) {
+			System.out.println(b);
+		}
+	}
+}
---
+++ javasecodes/src/ders5/dovrler/PrimitiveTypeConversion.java
@@ -0,0 +1,19 @@
+package ders5.dovrler;
+
+public class PrimitiveTypeConversion {
+ public static void main(String[] args) {
+	short s=69;
+	byte b=(byte)s;
+	System.out.println(b);
+	short a=589;
+	byte a2=(byte)a;
+	System.out.println(a2);
+	long l=458;
+	int l2=(int)l;
+	System.out.println(l2);
+	long k=52523635483L;
+	int k2=(int)k;
+	System.out.println(k2);
+	
+}
+}
