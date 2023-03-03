8adaf79742d39f0831a8414bfab4e66a88aca2b6
AnarHummetov <117859842+AnarHummetov@users.noreply.github.com>
2023-03-03 06:44:41 -0800

    spring-web-mvc_homework project created

---
+++ spring/spring-web-mvc-anar-ev/src/main/java/az/bakucode/springwebmvcanarev/SpringWebMvcAnarEvApplication.java
@@ -0,0 +1,13 @@
+package az.bakucode.springwebmvcanarev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebMvcAnarEvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebMvcAnarEvApplication.class, args);
+	}
+
+}
---
+++ spring/spring-web-mvc-anar-ev/src/test/java/az/bakucode/springwebmvcanarev/SpringWebMvcAnarEvApplicationTests.java
@@ -0,0 +1,13 @@
+package az.bakucode.springwebmvcanarev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebMvcAnarEvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
