111e927ab698f03080865db66a70072a1a36b290
AnarHummetov <117859842+AnarHummetov@users.noreply.github.com>
2023-05-02 23:59:01 +0400

    Fayl qovlugunun yaradılması

--- spring/bookshopping/src/main/java/az/developia/bookshopping/file/StorageFileNotFoundException.java
+++ spring/bookshopping/src/main/java/az/developia/bookshopping/file/StorageFileNotFoundException.java
@@ -1,5 +1,11 @@
 package az.developia.bookshopping.file;
 
-public class StorageFileNotFoundException {
+public class StorageFileNotFoundException extends StorageException {
 
+	public StorageFileNotFoundException(String message) {
+		super(message);
+	}
+	public StorageFileNotFoundException(String message,Throwable cause){
+		super(message,cause);
+	}	
 }
