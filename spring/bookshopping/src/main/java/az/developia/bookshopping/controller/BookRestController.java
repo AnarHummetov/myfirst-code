be2d61cfd0ff23d53112c88d8d3f72782b30df2c
AnarHummetov <117859842+AnarHummetov@users.noreply.github.com>
2023-05-10 17:59:20 +0400

    Alıcının kitabı ada görə axtarması

--- spring/bookshopping/src/main/java/az/developia/bookshopping/controller/BookRestController.java
+++ spring/bookshopping/src/main/java/az/developia/bookshopping/controller/BookRestController.java
@@ -5,11 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import az.developia.bookshopping.dao.BookDao;
 import az.developia.bookshopping.model.Book;
+import az.developia.bookshopping.model.SearchModel;
 
 @RestController
 @RequestMapping(path="/rest/books")
@@ -28,4 +31,9 @@ public class BookRestController {
 		return bookDao.findById(id).get();
 	}
 
+	@PostMapping(path = "/search")
+	public List<Book> findAllSearch(@RequestBody SearchModel search){
+		return bookDao.findAllSearch(search.getSearch());
+	}
+	
 }
---
+++ spring/bookshopping/src/main/java/az/developia/bookshopping/model/SearchModel.java
@@ -0,0 +1,17 @@
+package az.developia.bookshopping.model;
+
+public class SearchModel {
+	
+	private String search;
+
+	public String getSearch() {
+		return search;
+	}
+
+	public void setSearch(String search) {
+		this.search = search;
+	}
+	
+	
+
+}
